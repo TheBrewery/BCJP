a4baea2dbdee1f5f870102b51717a53bbbec56fd
Jim Hildensperger <jhildensperger@gmail.com>
2015-06-02 22:49:40 -0700

    initial commit

---
+++ bjcp_2015.js
@@ -0,0 +1,53 @@
+{
+    "styles": [
+        {
+            "style_name": "LIGHT LAGER",
+            "substyles": [
+                {
+                    "substyle_name": "American Light Lager",
+                    "aroma": "Low to no malt aroma, although it can be perceived as grainy, sweet, or corn-like if present. Hop aroma is light to none, with a spicy or floral hop character if present. While a clean fermentation character is desirable, a light amount of yeast character (particularly a light apple fruitiness) is not a fault. Light DMS is not a fault.",
+                    "appearance": "Very pale straw to pale yellow color. White, frothy head seldom persists. Very clear.",
+                    "flavor": "Relatively neutral palate with a crisp and dry finish and a low to very low grainy or corn-like flavor that might be perceived as sweetness due to the low bitterness. Hop flavor ranges from none to low levels, and can have a floral, spicy, or herbal quality (although rarely strong enough to detect). Low to very low hop bitterness. Balance may vary from slightly malty to slightly bitter, but is relatively close to even. High levels of carbonation may accentuate the crispness of the dry finish. Clean lager fermentation character.",
+                    "mouthfeel": "Very light (sometimes watery) body. Very highly carbonated with slight carbonic bite on the tongue.",
+                    "overall_impression": "Highly carbonated, very light-bodied, nearly flavorless lager designed to be consumed very cold. Very refreshing and thirst quenching.",
+                    "comments": "Designed to appeal to as broad a range of the general public as possible. Strong flavors are a fault.",
+                    "history": "Coors briefly made a light lager in the early 1940s. Modern versions were first produced by Rheingold in 1967 to appeal to diet-conscious drinkers, but only became popular starting in 1973 after Miller Brewing acquired the recipe and marketed the beer heavily to sports fans with the “tastes great, less filling” campaign. Beers of this genre became the largest sellers in the United States in the 1990s.",
+                    "characteristic_ingredients": "Two- or six-row barley with high percentage (up to 40%) of rice or corn as adjuncts. Additional enzymes can further lighten the body and lower carbohydrates.",
+                    "commercial_examples": [
+                        "Keystone Light",
+                        "Michelob Light",
+                        "Bitburger Light",
+                        "Sam Adams Light",
+                        "Heineken Premium Light",
+                        "Miller Lite",
+                        "Bud Light",
+                        "Coors Light",
+                        "Baltika #1 Light",
+                        "Old Milwaukee Light",
+                        "Amstel Light"
+                    ],
+                    "tags": [
+                        "session-strength",
+                        "pale-color",
+                        "bottom-fermented",
+                        "lagered",
+                        "north-america",
+                        "traditional-style",
+                        "pale-lager-family",
+                        "balanced"
+                    ],
+                    "ibu_low": 8,
+                    "ibu_high": 12,
+                    "srm_low": 2,
+                    "srm_high": 3,
+                    "original_gravity_low": 1.028,
+                    "original_gravity_high": 1.04,
+                    "final_gravity_low": 0.998,
+                    "final_gravity_high": 1.008,
+                    "abv_low": 2.8,
+                    "abv_high": 4.2
+                }
+            ]
+        }
+    ]
+}
